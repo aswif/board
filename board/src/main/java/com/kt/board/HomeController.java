69f37fb29e0352471f60d74a5f4b43d5aa60bf14
재진 <@192.168.43.43>
2016-12-13 17:29:12 +0900

    sddsd

--- board/src/main/java/com/kt/board/HomeController.java
+++ board/src/main/java/com/kt/board/HomeController.java
@@ -22,7 +22,7 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	//test - jjjj
+	//test - jjjj aaa
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
